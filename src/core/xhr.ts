0fe60ecf59f7e7da94b37469ed228239086fb7e0
guxiansheng <1352507209@qq.com>
2019-06-15 12:55:02 +0800

    自动化部署

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -124,6 +124,10 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         cancelToken.promise.then(reason => {
           request.abort();
           reject(reason)
+        }).catch(
+          /* istanbul ignore next */ 
+          () => {
+          // doNoting
         })
       }
     }
